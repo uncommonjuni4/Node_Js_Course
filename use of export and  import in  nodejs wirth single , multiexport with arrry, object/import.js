a774aa1e913a3c04c954bde6fac49cb6239fad42
Muhammad Junaid Hassan <chjunaidhassan95@gmail.com>
2026-09-02 13:18:52 +0500

    multi expport and  import
    multi expport and  import

---
+++ use of export and  import in  nodejs wirth single , multiexport with arrry, object/import.js
@@ -0,0 +1,8 @@
+const  calculator = require('./exportfile');
+
+const {add , sub , multi , div}= calculator;
+
+console.log(add(10  , 10));
+console.log(sub(10  , 10));
+console.log(multi(10  , 10));
+console.log(div(10  , 10));
