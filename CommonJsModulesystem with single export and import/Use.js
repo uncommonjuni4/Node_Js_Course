a552701d26af8980c21b33c1f2b4864d5ff3c314
Muhammad Junaid Hassan <chjunaidhassan95@gmail.com>
2026-09-02 13:17:39 +0500

    single export
    single export

---
+++ CommonJsModulesystem with single export and import/Use.js
@@ -0,0 +1,8 @@
+const add = require('./sum');
+
+const dif = require('./sub');
+
+
+console.log(add(12 , 4));
+console.log(dif(10,2));
+
---
+++ CommonJsModulesystem with single export and import/sub.js
@@ -0,0 +1,6 @@
+const  sub = (a , b)=>{
+     return a - b ;
+}
+
+
+module.exports = sub;
